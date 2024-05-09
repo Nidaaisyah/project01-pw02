0a739565ea25fa7066248215aba9d1215da955d3
Nidaaisyah <146088230+Nidaaisyah@users.noreply.github.com>
2024-05-09 08:56:41 +0700

    Add files via upload

---
+++ login.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login From</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css" integrity="sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N" crossorigin="anonymous">
+</head>
+<body>
+
+<?php
+     if(isset($_POST['submit'])){
+        require_once 'dbkoneksi.php';
+
+        $user = $dbh->prepare("SELECT * FROM users WHERE email = ? AND password = ?");
+        $user->execute([
+            $_POST['email'], $_POST['password']
+        ]);
+
+        $count = $user->rowCount(); //untuk memastikan apakah user tersedia atau tidak
+
+        if($count > 0) {
+            session_start();
+
+            $_SESSION['user'] = $user->fetch();
+             header("location:index.php");
+        } else { //jika gagal login
+            header("location:login.php");
+        }
+    }
+?>
+    <div class="container mt-5">
+        <h2 class="text-center">Login</h2>
+        <form action=" " method="POST">
+            <div class="form-group row">
+                <label for="email" class="col-4 col-form-label">Email</label>
+                <div class="col-8">
+                    <input id="email" name="email" type="email" class="form-control" requered>
+                </div>
+            </div>
+            <div class="form-group row">
+                <label for="password" class="col-4 col-form-label">Password</label>
+                <div class="col-8">
+                    <input id="password" name="password" type="password" class="form-control" requered>
+                </div>
+            </div>
+            <div class="form-group row">
+                <div class="offset-4 col-8">
+                    <button name="submit" type="submit" class="btn btn-primary">Submit</button>
+                </div>
+            </div>
+        </form>
+    </div>
+    <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-Fy6S3B9q64WdZWQUiU+q4/2Lc9npb8tCaSX9FK7E8HnRr0Jz8D6OP9dO5Vg3Q9ct" crossorigin="anonymous"></script>
+</body>
+</html>
